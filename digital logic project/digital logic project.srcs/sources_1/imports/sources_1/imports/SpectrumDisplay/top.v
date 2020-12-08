d2e463a138d9f0c73aab4dc2616afc42055a9a05
Nick Palladino <npallad@bu.edu>
2020-12-08 14:12:00 -0500

    Frequency register/bin sync
    Fixed connection between frequency registers and FFT output bins

--- digital logic project/digital logic project.srcs/sources_1/imports/sources_1/imports/SpectrumDisplay/top.v
+++ digital logic project/digital logic project.srcs/sources_1/imports/sources_1/imports/SpectrumDisplay/top.v
@@ -142,6 +142,7 @@ module top
     wire sync;
     reg [7:0] bin = 0;
 
+
     // update first BINS bins, syncing off the fft sync output
     localparam BIN_DIV = 7;
     always @(posedge clk_16m) begin
@@ -164,7 +165,11 @@ module top
     wire [WIDTH-1:0] decimated_data;
     decimator #(.WIDTH(WIDTH)) decimator_0(.clk(clk_16m), .ce(adc_ready), .data_in(adc_data), .data_out(decimated_data), .new_sample(fft_ce));
     fftmain fft_0(.i_clk(clk_16m), .i_reset(~reset), .i_ce(fft_ce), .i_sample({decimated_data, sample_imag}), .o_result({output_real, output_imag}), .o_sync(sync));
-    
+
+   assign FFT_out = bin;
+   
+   
+   
     // pwm and gamma correction for each LED
     generate
         genvar i;
