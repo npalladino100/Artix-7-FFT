38aaf61db350e98d50254fbcc1a8a2bc3041ff3e
saabanthony <74400483+saabanthony@users.noreply.github.com>
2020-12-09 22:39:21 -0500

    Update EC311fft.m

--- Matlab/EC311fft.m
+++ Matlab/EC311fft.m
@@ -11,4 +11,7 @@ b=getaudiodata(a);
 c=fft(b);
 
 plot(abs(c))
+title('Plot of frequency-domain spectrum corresponding to recorded audio')
+xlabel('Frequency (in rad/s)')
+ylabel('Magnitude')
 axis([0 20000 0 max(abs(c))]);
