6dc08b7c3a2b9fdf7d645bd99965ea0e22f3d16c
petercrary <74377937+petercrary@users.noreply.github.com>
2020-12-09 22:20:19 -0500

    Update adc.v

--- Artix-7-FFT/Artix-7-FFT.srcs/sources_1/new/adc.v
+++ Artix-7-FFT/Artix-7-FFT.srcs/sources_1/new/adc.v
@@ -7,54 +7,40 @@ module adc (
 	    output reg 	      ready,
 	    input wire 	      adc_sd,
 	    output reg [11:0] data
-	    //    output reg [11:0] max
 	    );
    
    initial begin
       ready = 0;
       data = 0;
-      //        max = 0;
    end
    
-   // outputs only valid not in reset
    assign adc_clk = clk;
    assign adc_cs = (cnt == 0 && reset == 0) ? 1 : 0; 
    
    reg [11:0] serial_data = 0;
    reg [3:0]  cnt = 0;
-   //    reg capture = 0;
    
    always @(posedge clk) begin
       
       if(reset) begin
-	 
-	 //            max <= 0;
          serial_data <= 12'b0;
          cnt <= 0;
          data <= 12'b0;
          ready <= 0;
-	 
       end else begin
 	 
          cnt<=cnt+1;
-	 
-	 //            if(cnt == 0) begin
-	 //                ready <= 0;
-	 //            end
 	 
          if(cnt > 3 && cnt <= 15)
            serial_data <= { serial_data[10:0], adc_sd };
          if(cnt == 3)
            serial_data <= 0;
-	 
          if(cnt == 0) begin
             data <= serial_data;
             ready <= 1;
-	    //                if(serial_data > max)
-	    //                    max <= serial_data;
          end
 	 
-         // ready signal is only valid for 1 clock, used for clock enable
+         // ready signal only high for one clock
          if(cnt == 1) 
            ready <= 0;
       end
